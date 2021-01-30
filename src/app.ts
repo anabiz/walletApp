4ea62f81283ec3f57c33c84861eaeeeadfffd6b5
anabiz <anabizconcept9@gmail.com>
2021-01-30 13:58:46 +0100

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express, { Request, Response, NextFunction } from "express";
 
 import logger from "morgan";
 import Route from "./routes"; 
-import { checkValidJsonPayload } from "./middleware/checkValidJsonPayload";
 
 
 const app = express();
@@ -12,7 +11,6 @@ app.disable("x-powered-by");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(checkValidJsonPayload);
 app.use("/", Route);
 
 // catch 404 and forward to error handler
